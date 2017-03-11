8a9f358be0c03ded34d3ea2ad1936d010946cacc
David <alienyidavid4christ@gmail.com>
2017-03-11 12:39:45 -0800

    add functionality to return different comparison value to 100 if argument is a number

--- src/dataTypes.js
+++ src/dataTypes.js
@@ -1,9 +1,26 @@
 function dataTypes(data){
-	//checking the type of the "data" and returns its length if it is a string.
+	//check the type of the input(data) and returns its length if it is a string.
 	if(typeof(data) === 'string'){
 		return data.length;
 	}
+	//check the type of the input(data) and returns its value  if it is a boolean.
 	else if(typeof(data) === 'boolean'){
 		return data;
 	}
+	//checks if the argument pass in is a number
+	else if(typeof(data) === 'number'){
+		//returns the string ‘less than 100’ if the number(data) is less than 100
+		if(data < 100){
+			return  "less than 100";
+		}
+		//returns the string ‘more than 100’ if the number(data) is less than 100
+		else if(data > 100){
+			return "more than 100";
+		}
+		//returns 'equal to 100' if number(data) is equal to 11
+		else{
+			return "equal to 100";
+		}
+	}
+	
 }
